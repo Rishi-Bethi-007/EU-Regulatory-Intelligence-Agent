e490c1d6657ca06a5a5bcacd10bdc1c09727f145
Rishi kumar Bethi <rishibethi007.com>
2026-04-09 10:12:15 +0200

    feat: add Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { inject } from '@vercel/analytics'
 import App from './App'
 import { AuthProvider } from './hooks/useAuth'
 import './index.css'
 import './print.css'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
